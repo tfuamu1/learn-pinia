5397fab95d29d23b8ea60ded285b33137ced49f4
thuyphamthu <thuyphamthu1509@gmail.com>
2023-11-02 00:53:19 +0900

    App.vueにストアを呼び出す

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,28 @@
-<script setup>
+<!-- <script setup>
 //定義したストアの関数をインポート
-import { useCounterStore } from '@/stores/counter'
+import { useCounterStore } from '@/stores/counter.js'
 //インポートした関数を呼び出してストアをインスタンス化（ストアを生成）して変数に代入
 const store = useCounterStore() //setup()内でuseXxxxStore()を実行
+</script> -->
+
+<script>
+import { useCounterStore } from '@/stores/counter.js';
+export default {
+  setup() {
+    //インポートした関数を呼び出してストアをインスタンス化して変数に代入
+    const store = useCounterStore() //setup()内でuseXxxxStore()を実行
+    return {
+      //テンプレートで仕様するストアのインスタンス全体を返すことができます
+      store,
+    }
+  },
+}
 </script>
 
 <template>
   <h1>Let's try Pinia!</h1>
+  <!-- 生成したストアを介してcounterにアクセス -->
+  <p>store.counter : {{ store.counter }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
