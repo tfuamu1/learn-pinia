993e9e86389a8e0d954bc141b53740e8ea4258ee
thuyphamthu <thuyphamthu1509@gmail.com>
2023-11-02 10:16:24 +0900

    作成したコンポーネントをアプリで呼び出す

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<!-- <script setup>
+<script setup>
 //定義したストアの関数をインポート
 import { useCounterStore } from '@/stores/counter.js'
+//作成したコンポーネントをインポート
+import PiniaSample from './components/PiniaSample.vue';
 //インポートした関数を呼び出してストアをインスタンス化（ストアを生成）して変数に代入
 const store = useCounterStore() //setup()内でuseXxxxStore()を実行
-</script> -->
+</script>
 
-<script>
+<!-- <script>
 import { useCounterStore } from '@/stores/counter.js';
 export default {
   setup() {
@@ -17,12 +19,26 @@ export default {
     }
   },
 }
-</script>
+</script> -->
 
 <template>
-  <h1>Let's try Pinia!</h1>
+  <div>
+  <h1 class="h1">Let's try Pinia!</h1>
   <!-- 生成したストアを介してcounterにアクセス -->
-  <p>store.counter : {{ store.counter }}</p>
+  <p class="counter">store.counter : {{ store.counter }}</p>
+  <!-- 追加したコンポーネント -->
+  <PiniaSample/>
+</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.h1 {
+  color: darkgreen;
+  font-size: 2.6rem;
+}
+
+.counter {
+font-size: 1.2rem;
+}
+
+</style>
