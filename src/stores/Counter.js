9096ec965d8467c7c9a6da6797dcd80997e10607
thuyphamthu <thuyphamthu1509@gmail.com>
2023-11-02 10:17:06 +0900

    第2引数にデータを追加

--- src/stores/Counter.js
+++ src/stores/Counter.js
@@ -1,25 +1,36 @@
 // import { ref,computed } from "vue"
-import { defineStore } from "pinia"
+import { defineStore } from 'pinia'
 
 //defineStore() の第2引数に Options オブジェクトを指定して定義する例
 export const useCounterStore = defineStore('counter', {
-    //初期状態（初期値）を返す関数として定義
-    state: () => ({
-        counter: 3
-    }),
-    //算出プロパティ
-    getters: {
-        //第１引数にstateを受け取る
-        doubleCount: (state) => state.counter * 2,
+  //初期状態（初期値）を返す関数として定義
+  state: () => ({
+    counter: 0,
+    //以下のデータを追加
+    greeting: 'こんにちは！',
+    user: {
+      name: 'ペリー'
+    }
+  }),
+  //算出プロパティ
+  getters: {
+    //第１引数にstateを受け取る
+    doubleCount: (state) => state.counter * 2,
+    //以下の算出プロパティを追加
+    tripleCount: (state) => state.counter * 3
+  },
+  //stateの値を更新する関数
+  actions: {
+    //thisを利用するのでアロー関数は使えない
+    increment() {
+      //ストアのインスタンスにthisでアクセス
+      this.counter++
     },
-    //stateの値を更新する関数
-    actions: {
-        //thisを利用するのでアロー関数は使えない
-        increment() {
-            //ストアのインスタンスにthisでアクセス
-            this.counter++
-        },
-    },
+    //以下の関数を追加
+    decrement() {
+      this.counter--
+    }
+  }
 })
 
 // // defineStore()の第2引数にSetup関数（リアクティブなオブジェクトを返す関数）で定義する例
