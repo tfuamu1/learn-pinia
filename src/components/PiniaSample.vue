70b5cab880591e69f7e97c3b2a1b5cb51512ce66
thuyphamthu <thuyphamthu1509@gmail.com>
2023-11-02 10:16:50 +0900

    コンポーネントを作成する

---
+++ src/components/PiniaSample.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter.js'
+//インポートした関数を呼び出してストアをインスタンス化して変数に代入
+const store = useCounterStore()
+</script>
+
+<template>
+  <h2>Pinia Sample</h2>
+  <p>store.doubleCount: {{ store.doubleCount }}</p>
+  <p>store.tripleCount: {{ store.tripleCount }}</p>
+  <div>
+    <button @click="store.increment">Increment</button>
+    <button @click="store.decrement">Decrement</button>
+  </div>
+  <p>{{ store.greeting }} {{ store.user.name }}</p>
+  <input v-model="store.user.name"/>
+</template>
